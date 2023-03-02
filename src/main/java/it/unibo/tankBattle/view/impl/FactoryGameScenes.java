e271f74f570f4979d703cff8c890815f467739ee
Emanuele_Martelli <emanuele.martelli5studio.unibo.it>
2023-03-02 11:05:31 +0100

    modification of the view with a more accurate implementation of the menu

--- src/main/java/it/unibo/tankBattle/view/impl/FactoryGameScenes.java
+++ src/main/java/it/unibo/tankBattle/view/impl/FactoryGameScenes.java
@@ -2,37 +2,68 @@ package it.unibo.tankBattle.view.impl;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
-
 import it.unibo.tankBattle.view.api.Graphics;
-
+import java.awt.Color;
+import java.awt.BorderLayout;
 import java.awt.Toolkit;
 import java.awt.Dimension;
 
 public class FactoryGameScenes {
     private final Graphics view;
+    private final Dimension screenSize;
 
     protected FactoryGameScenes(Graphics view) {
         this.view = view;
+        screenSize = view.getSize();
     }
 
-    public JPanel menu () {
-        final JPanel menuPanel = new JPanel();
+    protected JPanel menu () {
+        final JPanel menuPanel = new JPanel(new BorderLayout());
+        menuPanel.setBackground(Color.BLACK);
+    
+        final JPanel centerPanel = new JPanel(new BorderLayout());
+        setSize(centerPanel, screenSize.width/3, screenSize.height/2);
+        setTrasparent(centerPanel);
+
+        final JPanel northPanel = new JPanel();
+        setSize(northPanel, screenSize.width, screenSize.height/4);
+        setTrasparent(northPanel);
+
+        final JPanel southPanel = new JPanel();
+        setSize(southPanel, screenSize.width, screenSize.height/4);
+        setTrasparent(southPanel);
+
+        final JPanel westPanel = new JPanel();
+        setSize(westPanel, screenSize.width/3, screenSize.height/2);
+        setTrasparent(westPanel);
+
+        final JPanel eastPanel = new JPanel();
+        setSize(eastPanel, screenSize.width/3, screenSize.height/2);
+        setTrasparent(eastPanel);
+
         final JButton playButton = new JButton("Play");
         final JButton tutorialButton = new JButton("Tutorial");
         
-        menuPanel.setMaximumSize(Toolkit.getDefaultToolkit().getScreenSize());
-        playButton.setPreferredSize(new Dimension(1000,100));
+        playButton.setPreferredSize(new Dimension(view.getSize().width/3, view.getSize().height/6));
         tutorialButton.addActionListener(e ->{
             view.tutorial();
         });
-        tutorialButton.setPreferredSize(new Dimension(1000,100));
-        menuPanel.add(playButton);
-        menuPanel.add(tutorialButton);
+        tutorialButton.setPreferredSize(new Dimension(view.getSize().width/3, view.getSize().height/6));
+
+        centerPanel.add(playButton, BorderLayout.NORTH);
+        centerPanel.add(tutorialButton, BorderLayout.SOUTH);
+
+        menuPanel.add(northPanel, BorderLayout.NORTH);
+        menuPanel.add(westPanel, BorderLayout.WEST);
+        menuPanel.add(eastPanel, BorderLayout.EAST);
+        menuPanel.add(southPanel, BorderLayout.SOUTH);
+        menuPanel.add(centerPanel, BorderLayout.CENTER);
+        
         return menuPanel;
     }
 
-    public JPanel tutorial () {
-        /*final JPanel tutorialPanel = new JPanel();
+    protected JPanel tutorial () {
+        final JPanel tutorialPanel = new JPanel();
         final JButton backButton = new JButton("Back");
         tutorialPanel.setMaximumSize(Toolkit.getDefaultToolkit().getScreenSize());
         backButton.setPreferredSize(new Dimension(100,100));
@@ -41,23 +72,18 @@ public class FactoryGameScenes {
         });
         
         tutorialPanel.add(backButton);
-        return tutorialPanel;*/
-        final JPanel menuPanel = new JPanel();
-        final JButton playButton = new JButton("Play2");
-        final JButton tutorialButton = new JButton("Tutorial2");
-        
-        menuPanel.setMaximumSize(Toolkit.getDefaultToolkit().getScreenSize());
-        playButton.setPreferredSize(new Dimension(1000,100));
-        tutorialButton.addActionListener(e ->{
-            view.tutorial();
-        });
-        tutorialButton.setPreferredSize(new Dimension(1000,100));
-        menuPanel.add(playButton);
-        menuPanel.add(tutorialButton);
-        return menuPanel;
+        return tutorialPanel;
     }
 
-    public JPanel gameScene () {
+    protected JPanel gameScene () {
         return null;
+    }
+
+    private void setSize(JPanel panel, int width, int height) {
+        panel.setPreferredSize(new Dimension(width, height));
+    }
+
+    private void setTrasparent(JPanel panel) {
+        panel.setBackground(new Color(0,0,0,125));
     }
 }
--- src/main/java/it/unibo/tankBattle/view/impl/GraphicsImpl.java
+++ src/main/java/it/unibo/tankBattle/view/impl/GraphicsImpl.java
@@ -2,28 +2,40 @@ package it.unibo.tankBattle.view.impl;
 
 
 import javax.swing.JPanel;
-import java.awt.Toolkit;
+import javax.swing.text.View;
 
+import java.awt.Toolkit;
+import java.awt.Dimension;
 import it.unibo.tankBattle.common.P2d;
 import it.unibo.tankBattle.view.api.Graphics;
 
 public class GraphicsImpl extends Graphics {
     private final FactoryGameScenes factory;
+    private final JPanel mainPanel;
     private final JPanel menu;
     private final JPanel tutorial;
     private JPanel gameScene = new JPanel();
     private JPanel courrentPanel = new JPanel();
 
     public GraphicsImpl() {
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
         this.factory = new FactoryGameScenes(this);
         menu = factory.menu();
         tutorial = factory.tutorial();
-        this.setContentPane(menu);
+        mainPanel = new JPanel();
+        mainPanel.setPreferredSize(this.getSize());
+
+        this.setContentPane(mainPanel);
+        mainPanel.add(menu);
+        mainPanel.add(tutorial);
         courrentPanel = menu;
-        tutorial.setVisible(true);
-        this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+
+        menu.setVisible(true);
+        tutorial.setVisible(false);
+
         this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("TankBattle");
     }
 
     @Override
@@ -46,16 +58,18 @@ public class GraphicsImpl extends Graphics {
 
     @Override
     public void tutorial() {
-        this.setContentPane(tutorial);
+        courrentPanel.setVisible(false);
         courrentPanel = tutorial;
+        courrentPanel.setVisible(true);
         this.repaint();
     }
 
     @Override
     public void menu() {
-        this.remove(courrentPanel);
+        courrentPanel.setVisible(false);
         courrentPanel = menu;
-        this.setContentPane(courrentPanel);
+        courrentPanel.setVisible(true);
+        this.repaint();
     }
     
 }
