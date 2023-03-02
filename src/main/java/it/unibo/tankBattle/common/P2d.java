048509aaa1bdfa738eaaa8627c3aac2d250293f5
Tomas Ventrucci <tomas.ventrucci@studio.unibo.it>
2023-03-02 16:38:06 +0100

    implemented P2d and V2d

--- src/main/java/it/unibo/tankBattle/common/P2d.java
+++ src/main/java/it/unibo/tankBattle/common/P2d.java
@@ -1,8 +1,37 @@
 package it.unibo.tankBattle.common;
 
+/**
+ * 2 dimentional point
+ */
 public class P2d {
     
-    /*
-     * TODO
-     */
+    private int x;
+    private int y;
+
+    P2d(final int x, final int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public P2d sum(final V2d v){
+        return new P2d(x + v.x, y + v.y);
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    
+    public int getY(){
+        return this.y;
+    }
+
+    public V2d sub(final P2d v){
+        return new V2d(x - v.x, y - v.y);
+    }
+
+    public String toString(){
+        return "P2d("+x+","+y+")";
+    }
+
 }
