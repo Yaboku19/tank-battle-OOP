826ee84e48375224d0496121ddd11cbd38304c51
Tomas Ventrucci <tomas.ventrucci@studio.unibo.it>
2023-03-02 16:23:01 +0100

    created Movement and Shoot classes

---
+++ src/main/java/it/unibo/tankBattle/common/input/impl/Movement.java
@@ -0,0 +1,16 @@
+package it.unibo.tankBattle.common.input.impl;
+
+import it.unibo.tankBattle.common.input.api.Command;
+
+/**
+ * That class manage the movement of the player
+ */
+public class Movement implements Command{
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
---
+++ src/main/java/it/unibo/tankBattle/common/input/impl/Shoot.java
@@ -0,0 +1,15 @@
+package it.unibo.tankBattle.common.input.impl;
+
+import it.unibo.tankBattle.common.input.api.Command;
+/**
+ * That class manage shoot of the player
+ */
+public class Shoot implements Command{
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
