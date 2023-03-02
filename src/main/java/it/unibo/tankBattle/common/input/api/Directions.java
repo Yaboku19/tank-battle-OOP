be2e5fa5c7fd43b5349e07227c48887b4e469117
Tomas Ventrucci <tomas.ventrucci@studio.unibo.it>
2023-03-02 16:21:33 +0100

    created Command and InputController Interface and Directions enum

---
+++ src/main/java/it/unibo/tankBattle/common/input/api/Directions.java
@@ -0,0 +1,8 @@
+package it.unibo.tankBattle.common.input.api;
+
+/**
+ * Directions that can be used by the player
+ */
+public enum Directions {
+    UP, RIGHT, DOWN, LEFT;
+}
--- src/main/java/it/unibo/tankBattle/common/input/api/InputController.java
+++ src/main/java/it/unibo/tankBattle/common/input/api/InputController.java
@@ -1,8 +1,16 @@
 package it.unibo.tankBattle.common.input.api;
 
+import java.util.Queue;
+
+/**
+ * This interface represent an InutController that gets notified when an input
+ * occours.
+ */
 public interface InputController {
-    
-    /*
-     * TODO
+    /**
+     * Return the queue of commands to be executed.
+     * 
+     * @return the commands queue
      */
+    Queue<Command> getCommands();
 }
