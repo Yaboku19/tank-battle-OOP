4cf87ddff91a2db5cf152408dc86e16e234a7749
Riccardo Frascio <riccardo.frascio@studio.unibo.it>
2023-03-03 19:33:05 +0100

    add getter in Directions

--- src/main/java/it/unibo/tankBattle/common/input/api/Directions.java
+++ src/main/java/it/unibo/tankBattle/common/input/api/Directions.java
@@ -12,6 +12,14 @@ public enum Directions {
     int x;
     int y;
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
     Directions(int x, int y) {
         this.x = x;
         this.y = y;
