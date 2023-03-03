7c26ed49dc1ed45f7c82bfd077095c4a710e0980
Tomas Ventrucci <tomas.ventrucci@studio.unibo.it>
2023-03-03 15:32:33 +0100

    resolving merge conflict

--- src/main/java/it/unibo/tankBattle/controller/api/GameEngine.java
+++ src/main/java/it/unibo/tankBattle/controller/api/GameEngine.java
@@ -2,12 +2,8 @@ package it.unibo.tankBattle.controller.api;
 
 public interface GameEngine {
     public void play();
-<<<<<<< HEAD
 
     public void processInput();
-
-=======
     
     public void startGame();
->>>>>>> ba53e2aa0409289c75dbedd7386837f1a055003b
 }
