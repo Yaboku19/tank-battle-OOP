f4496b91fe59e43932b3d2a37f4abc10d6a0e493
Emanuele_Martelli <emanuele.martelli5studio.unibo.it>
2023-03-03 14:27:20 +0100

    new Class WorldImpl and GameStateImpl

--- src/main/java/it/unibo/tankBattle/controller/impl/BasicGameEngine.java
+++ src/main/java/it/unibo/tankBattle/controller/impl/BasicGameEngine.java
@@ -1,11 +1,14 @@
 package it.unibo.tankBattle.controller.impl;
 
 import it.unibo.tankBattle.controller.api.GameEngine;
+import it.unibo.tankBattle.model.api.GameState;
+import it.unibo.tankBattle.model.impl.GameStateImpl;
 import it.unibo.tankBattle.view.api.View;
 import it.unibo.tankBattle.view.impl.ViewImpl;
 
 public class BasicGameEngine implements GameEngine {
     private View view;
+    private GameState model;
 
     public BasicGameEngine() {
         view = new ViewImpl(this);
@@ -24,6 +27,11 @@ public class BasicGameEngine implements GameEngine {
     @Override
     public void startGame() {
         System.out.println("game started");
+        model = new GameStateImpl();
+        /*
+         * new instance of model
+         */
+        loop();
     }
     
 }
---
+++ src/main/java/it/unibo/tankBattle/model/impl/GameStateImpl.java
@@ -0,0 +1,36 @@
+package it.unibo.tankBattle.model.impl;
+
+import it.unibo.tankBattle.model.api.GameState;
+
+public class GameStateImpl implements GameState{
+    private final World world;
+
+    public GameStateImpl() {
+        this.world = new WorldImpl();
+    }
+
+    @Override
+    public void getScore() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getScore'");
+    }
+
+    @Override
+    public boolean isOver() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isOver'");
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void increaseScore() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'increaseScore'");
+    }
+    
+}
