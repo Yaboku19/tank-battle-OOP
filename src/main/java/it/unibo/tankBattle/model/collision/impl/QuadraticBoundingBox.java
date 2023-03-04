d9a0e846a71646d9f318f011815ce0a3a67659ba
Lucrezia Rettori <lucrezia.rettori@studio.unibo.it>
2023-03-04 17:01:40 +0100

    creation object area stream

--- src/main/java/it/unibo/tankBattle/model/collision/impl/QuadraticBoundingBox.java
+++ src/main/java/it/unibo/tankBattle/model/collision/impl/QuadraticBoundingBox.java
@@ -1,5 +1,7 @@
 package it.unibo.tankBattle.model.collision.impl;
 
+import java.util.stream.Stream;
+
 import it.unibo.tankBattle.common.P2d;
 import it.unibo.tankBattle.model.collision.api.BoundingBox;
 
@@ -21,11 +23,33 @@ public class QuadraticBoundingBox implements BoundingBox {
         return new P2d(this.center.getX() + (this.length / 2), this.center.getY() + (this.length / 2));
     }
 
+    private Stream<P2d> objectArea() {
+        return Stream.concat(
+            Stream.concat(
+                Stream
+                    .iterate(getUpLeftCorner(), x -> new P2d(getUpLeftCorner().getX() + 1, getUpLeftCorner().getY()))
+                    .limit(this.length)
+                ,
+                Stream
+                    .iterate(getBottomRightCorner(), x -> new P2d(getBottomRightCorner().getX() - 1, getUpLeftCorner().getY()))
+                    .limit(this.length)
+                ),
+            Stream.concat(
+                Stream
+                    .iterate(getUpLeftCorner(), x -> new P2d(getUpLeftCorner().getX(), getUpLeftCorner().getY() + 1))
+                    .limit(this.length)
+                ,
+                Stream
+                    .iterate(getBottomRightCorner(), x -> new P2d(getBottomRightCorner().getX(), getBottomRightCorner().getY() - 1))
+                    .limit(this.length)
+                )
+        );
+    }
+    
     @Override
     public boolean isColliding(P2d center, int length) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'isColliding'");
-
     }
 
 }
