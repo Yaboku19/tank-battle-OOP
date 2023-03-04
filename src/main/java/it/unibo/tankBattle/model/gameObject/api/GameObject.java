8b07c2007a8b4af8a6ab5448c7b0b069d3d97394
Riccardo Frascio <riccardo.frascio@studio.unibo.it>
2023-03-04 15:11:57 +0100

    add method resolveCollision in GameObject

--- src/main/java/it/unibo/tankBattle/model/gameObject/api/GameObject.java
+++ src/main/java/it/unibo/tankBattle/model/gameObject/api/GameObject.java
@@ -18,13 +18,19 @@ public interface GameObject {
 
     public int getLifePoints();
 
-    public void setDirection(Directions dir);
+    public int getMaxSpeed();
 
-    public void setPosition();
+    public void setDirection(Directions dir);
 
     public void hit(int damageReceive);
 
     public void move();
 
     public void stop();
+
+    public abstract boolean isAlive();
+
+    public abstract void update();
+
+    public abstract void resolveCollision();
 }
