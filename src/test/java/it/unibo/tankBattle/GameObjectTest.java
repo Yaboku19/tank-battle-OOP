1734566cbdd8f9876c7b9206673597e089a7e74c
Riccardo Frascio <riccardo.frascio@studio.unibo.it>
2023-03-04 17:23:19 +0100

    add method manageCollision, knockBack and few tests

--- src/test/java/it/unibo/tankBattle/GameObjectTest.java
+++ src/test/java/it/unibo/tankBattle/GameObjectTest.java
@@ -21,7 +21,7 @@ public class GameObjectTest {
     @org.junit.jupiter.api.BeforeEach
 	public void initFactory() {
 	this.factory = new FactoryGameObject();
-	tank = this.factory.simpleTank(1, new P2d(1,1), 100, 10);
+	tank = this.factory.simpleTank(1, new P2d(10,10), 100, 10);
 	bullet = this.factory.simpleBullet(20, tank);
 	obstacle = this.factory.simpleWall(new P2d(10, 20));
 	}
@@ -32,6 +32,18 @@ public class GameObjectTest {
 		assertTrue(tank.isAlive(), str);
 		tank.hit(90);
 		assertFalse(tank.isAlive(), str);
+
+	}
+
+	@org.junit.jupiter.api.Test
+	public void testCollision() {
+		/*assertEquals(tank.possibleCollision(new P2d(11, 0)), Directions.DOWN);
+		assertEquals(tank.possibleCollision(new P2d(11, 20)), Directions.UP);
+		assertEquals(tank.possibleCollision(new P2d(30, 11)), Directions.LEFT);
+		assertEquals(tank.possibleCollision(new P2d(0, 11)), Directions.RIGHT);
+		assertEquals(tank.possibleCollision(new P2d(0, 10)), Directions.RIGHT);*/
+		
+		
 
 	}
 
