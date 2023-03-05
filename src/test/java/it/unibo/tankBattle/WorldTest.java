f348c7eebad6d47af1d3e72f3ec3d5e1804c29a1
Emanuele_Martelli <emanuele.martelli5studio.unibo.it>
2023-03-05 20:52:42 +0100

    removed an import

--- src/test/java/it/unibo/tankBattle/WorldTest.java
+++ src/test/java/it/unibo/tankBattle/WorldTest.java
@@ -3,7 +3,6 @@ package it.unibo.tankBattle;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashSet;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import it.unibo.tankBattle.common.P2d;
