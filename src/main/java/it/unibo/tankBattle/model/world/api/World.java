d25eca11d19a7e2c32582d581ed608f36fda375a
Emanuele_Martelli <emanuele.martelli5studio.unibo.it>
2023-03-06 22:36:34 +0100

    added javaDock in World

--- src/main/java/it/unibo/tankBattle/model/world/api/World.java
+++ src/main/java/it/unibo/tankBattle/model/world/api/World.java
@@ -5,22 +5,51 @@ import it.unibo.tankBattle.common.input.api.Directions;
 import java.util.Set;
 import it.unibo.tankBattle.model.gameObject.api.GameObject;
 import it.unibo.tankBattle.model.gameState.api.Player;
-
+/**
+ * The classe where are saved all the GameObject of the Map.
+ */
 public interface World {
-
+    /**
+     *  method used for the update of all gameObject in World.
+     */
     public void update();
-
-    public void shot(Player player);
-
+    /**
+     * method for manage the collision between two GameObject.
+     * @param firsGameObject first GameObject
+     * @param secondGameObject secon GameObject
+     */
     public void collision(P2d firsGameObject, P2d secondGameObject);
-
+    /**
+     * return all the GameObject in World.
+     * @return all the GameObject
+     */
     public Set<GameObject> getEntities();
-
+    /**
+     * return all GameObject that are Walls.
+     * @return walls
+     */
     public Set<GameObject> getWalls();
-
+    /**
+     * return all GameObject that are Bullets.
+     * @return bullets
+     */
     public Set<GameObject> getBullets();
-
+    /**
+     * return a tank using a paramather Player.
+     * @param player used for identify the tank
+     * @return the tank
+     */
     public GameObject getTank(Player player);
-
+    /**
+     * method that give the command to shot at a tank.
+     * @param player used for identify the tank
+     */
+    public void shot(Player player);
+    /**
+     * method that give the command to change their direction, used for the movement,
+     *  in particular when the direction is NONE the tank will not move.
+     * @param direction direction that will be set in tank
+     * @param player used for identify the tank
+     */
     public void setDirection(Directions direction, Player player);
 }
