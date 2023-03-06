499998ad65576a8cb36aac48a1620b91178c92f9
Emanuele_Martelli <emanuele.martelli5studio.unibo.it>
2023-03-06 22:48:41 +0100

    added javaDock

--- src/main/java/it/unibo/tankBattle/model/gameState/api/Player.java
+++ src/main/java/it/unibo/tankBattle/model/gameState/api/Player.java
@@ -1,7 +1,13 @@
 package it.unibo.tankBattle.model.gameState.api;
 
 public interface Player {
+    /**
+     * Return the score of the player.
+     * @return the score
+     */
     public int getScore();
-
+    /**
+     * Add one to the sscore.
+     */
     public void incScore();
 }
--- src/main/java/it/unibo/tankBattle/model/world/api/World.java
+++ src/main/java/it/unibo/tankBattle/model/world/api/World.java
@@ -10,43 +10,43 @@ import it.unibo.tankBattle.model.gameState.api.Player;
  */
 public interface World {
     /**
-     *  method used for the update of all gameObject in World.
+     * Method used for the update of all gameObject in World.
      */
     public void update();
     /**
-     * method for manage the collision between two GameObject.
+     * Method for manage the collision between two GameObject.
      * @param firsGameObject first GameObject
      * @param secondGameObject secon GameObject
      */
     public void collision(P2d firsGameObject, P2d secondGameObject);
     /**
-     * return all the GameObject in World.
+     * Return all the GameObject in World.
      * @return all the GameObject
      */
     public Set<GameObject> getEntities();
     /**
-     * return all GameObject that are Walls.
+     * Return all GameObject that are Walls.
      * @return walls
      */
     public Set<GameObject> getWalls();
     /**
-     * return all GameObject that are Bullets.
+     * Return all GameObject that are Bullets.
      * @return bullets
      */
     public Set<GameObject> getBullets();
     /**
-     * return a tank using a paramather Player.
+     * Return a tank using a paramather Player.
      * @param player used for identify the tank
      * @return the tank
      */
     public GameObject getTank(Player player);
     /**
-     * method that give the command to shot at a tank.
+     * Method that give the command to shot at a tank.
      * @param player used for identify the tank
      */
     public void shot(Player player);
     /**
-     * method that give the command to change their direction, used for the movement,
+     * Method that give the command to change their direction, used for the movement,
      *  in particular when the direction is NONE the tank will not move.
      * @param direction direction that will be set in tank
      * @param player used for identify the tank
