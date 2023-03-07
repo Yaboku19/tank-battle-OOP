8b9cb678a472e80ad8fe9c1def4e7668904920c3
Tomas Ventrucci <tomas.ventrucci@studio.unibo.it>
2023-03-07 01:24:43 +0100

    try to import tank gif in view

--- src/main/java/it/unibo/tankBattle/view/impl/ViewImpl.java
+++ src/main/java/it/unibo/tankBattle/view/impl/ViewImpl.java
@@ -1,5 +1,8 @@
 package it.unibo.tankBattle.view.impl;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Toolkit;
 import java.awt.Image;
@@ -10,6 +13,7 @@ import it.unibo.tankBattle.controller.api.GameEngine;
 import it.unibo.tankBattle.view.api.View;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 
 
@@ -22,7 +26,13 @@ public class ViewImpl extends View implements KeyListener{
     private JPanel gameScenePanel = new JPanel();
     private JPanel courrentPanel = new JPanel();
     //private final Image imgBackGround = Toolkit.getDefaultToolkit().getImage("C:\\Users\\marte\\OneDrive\\Desktop\\OOP-Project\\src\\main\\java\\it\\unibo\\tankBattle\\view\\impl\\download.jpg");
+    //private final Image tankImage = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Tomas\\Documents\\Università\\2anno\\1\\OOP\\Progetto\\tank-battle-OOP\\src\\main\\java\\it\\unibo\\tankBattle\\view\\impl\\Resources\\tank1.gif");
+    Icon icon = new ImageIcon("C:\\Users\\Tomas\\Documents\\Università\\2anno\\1\\OOP\\Progetto\\tank-battle-OOP\\src\\main\\java\\it\\unibo\\tankBattle\\view\\impl\\Resources\\tank1.gif");
+    JLabel label = new JLabel(icon);
+    
+
     private final GameEngine controller;
+    /************************ */
     private P2d position = new P2d(50, 50);
 
     public ViewImpl(final GameEngine controller){
@@ -36,24 +46,24 @@ public class ViewImpl extends View implements KeyListener{
         mainPanel = new JPanel();
         mainPanel.setPreferredSize(this.getSize());
         mainPanel.setBackground(Color.BLACK);
-        /*************************************************** */
+        /***************************************************/ 
         this.setContentPane(new JPanel() {
             @Override
             public void paintComponent(Graphics g) {
-               super.paintComponent(g);
-               /*g.drawImage(imgBackGround, 0, 0, getWidth(), getHeight(), null);*/
-                g.drawRect(position.getX(), position.getY(), 50, 50);
+                super.paintComponent(g);
+                //g.drawImage(tankImage, -150, -150, getWidth(), getHeight(), null);
+                /*g.drawImage(imgBackGround, 0, 0, getWidth(), getHeight(), null);*/
+                /*g.drawRect(position.getX(), position.getY(), 50, 50);
                 g.fillRect(position.getX(), position.getY(), 50, 50);
-                g.setColor(Color.BLACK);
+                g.setColor(Color.BLACK);*/
             }
          });
-         this.setVisible(true);
          /***************************************************/
         mainPanel.add(menuPanel);
         mainPanel.add(tutorialPanel);
         mainPanel.add(gameChoosePanel);
         courrentPanel = menuPanel;
-        mainPanel.setVisible(false);/*********************/
+        //mainPanel.setVisible(false);/*********************/
         menuPanel.setVisible(false);
         tutorialPanel.setVisible(false);
         gameScenePanel.setVisible(false);
@@ -104,7 +114,11 @@ public class ViewImpl extends View implements KeyListener{
     public void chooseMenu() {
         courrentPanel.setVisible(false);
         courrentPanel = gameChoosePanel;
-        courrentPanel.setVisible(true);
+        this.setVisible(true);
+        //label.setPreferredSize(new Dimension(icon.getIconHeight(), icon.getIconWidth()));
+        this.add(label);
+        mainPanel.setVisible(false);
+        //courrentPanel.setVisible(true);
         this.repaint();
     }
 
