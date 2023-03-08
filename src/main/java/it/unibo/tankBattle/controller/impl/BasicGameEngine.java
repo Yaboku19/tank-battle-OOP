ae25a174164ed7ae8953cc86c0ac2d4e6e34c8c3
Emanuele_Martelli <emanuele.martelli5studio.unibo.it>
2023-03-08 15:51:38 +0100

    started gameLoop

--- src/main/java/it/unibo/tankBattle/controller/impl/BasicGameEngine.java
+++ src/main/java/it/unibo/tankBattle/controller/impl/BasicGameEngine.java
@@ -1,6 +1,7 @@
 package it.unibo.tankBattle.controller.impl;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import static java.awt.event.KeyEvent.*;
@@ -11,8 +12,8 @@ import it.unibo.tankBattle.model.gameState.api.Player;
 import it.unibo.tankBattle.view.api.View;
 import it.unibo.tankBattle.view.impl.ViewImpl;
 import it.unibo.tankBattle.common.Pair;
-
-import it.unibo.tankBattle.common.input.api.*;
+import it.unibo.tankBattle.common.input.api.Command;
+import it.unibo.tankBattle.common.input.api.InputController;
 import it.unibo.tankBattle.common.input.impl.KeyboardInputController;
 import it.unibo.tankBattle.common.input.impl.Movement;
 
@@ -21,6 +22,7 @@ public class BasicGameEngine implements GameEngine {
     private GameState model;
     private Queue<Pair<Player,Command>> commandQueue = new LinkedList<>();
     private HashMap<Player,InputController> controllers;
+    private Boolean isOver = false;
 
     public BasicGameEngine() {
         view = new ViewImpl(this);
@@ -30,19 +32,6 @@ public class BasicGameEngine implements GameEngine {
     public void play() {
         view.setVisible(true);
         view.bugSolve();
-    }
-
-    private void processInput() {
-        var cmd = commandQueue.poll();
-        /*cmd.execute(cmd);*/
-        /*for(var tank : model.getWorld().getTanks()){
-            tank.updateInput();
-        }*/
-        //throw new UnsupportedOperationException("Unimplemented method 'processInput'");
-    }
-
-    private void loop() {
-        
     }
 
     private void initGame(){
@@ -58,10 +47,43 @@ public class BasicGameEngine implements GameEngine {
     public void startGame() {
         System.out.println("game started");
         model = new GameStateImpl(this);
+        initGame();
         /*
          * new instance of model
          */
         loop();
+    }
+
+    private void loop() {
+        this.isOver = false;
+        while(!isOver) {
+            processInput();
+            update();
+            events();
+            render();
+            // time at each frame toDo
+        }
+    }
+
+    private void processInput() {
+        var cmd = commandQueue.poll();
+        /*cmd.execute(cmd);*/
+        /*for(var tank : model.getWorld().getTanks()){
+            tank.updateInput();
+        }*/
+        //throw new UnsupportedOperationException("Unimplemented method 'processInput'");
+    }
+
+    private void update() {
+        model.update();
+    }
+
+    private void events() {
+        model.resolveEvents(new HashSet<>());
+    }
+
+    private void render() {
+        view.repaint();
     }
 
     @Override
@@ -77,8 +99,7 @@ public class BasicGameEngine implements GameEngine {
 
     @Override
     public void endgame() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'endgame'");
+        this.isOver = true;
     }
     
 }
