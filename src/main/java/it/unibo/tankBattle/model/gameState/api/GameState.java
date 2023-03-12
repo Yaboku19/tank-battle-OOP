bef7dd3b71976fbd413bed1cffd01953bf4f58fe
Emanuele_Martelli <emanuele.martelli5studio.unibo.it>
2023-03-12 17:20:29 +0100

    change of the GameState

--- src/main/java/it/unibo/tankBattle/model/gameState/api/GameState.java
+++ src/main/java/it/unibo/tankBattle/model/gameState/api/GameState.java
@@ -1,69 +1,25 @@
 package it.unibo.tankBattle.model.gameState.api;
 
-import java.util.Set;
-import it.unibo.tankBattle.common.P2d;
-import it.unibo.tankBattle.common.Pair;
-import it.unibo.tankBattle.common.input.api.Directions;
-/**
- * Classe that is the middle between the GameEngine (Controller) and the World.
- */
-public interface GameState {
-    /**
-     * Give the input to thw world to update.
-     */
-    public void update();
-    /**
-     * Choose how the method of world have to be called for all the events. 
-     * @param events A set of events
-     */
-    public void resolveEvents(final Set<Pair<P2d, P2d>> events);
-    /**
-     * It is called by the world when a tank is dead.
-     * @param player the Player that own the tank
-     */
-    public void endGame(final Player player);
-    /**
-     * Used by the controller to send the inputCommand.
-     */
-    public void input();
+import java.util.stream.Stream;
 
-    /**
-     * 
-     * This method get each object's and return it's position and length in order to share
-     * less information to manage collision.
-     * 
-     * @return a set of pair of each object's position and length
-     */
-    public Set<Pair<P2d, Integer>> getPositionsAndLength();
-    /**
-     * Return the position of all the walls used for the view.
-     * @return a set of position
-     */
-    public Set<P2d> getWallPositions();
-    /**
-     * Return the position and the direction of all the bullets used for the view.
-     * @return a set of position and directions
-     */
-    public Set<Pair<P2d, Directions>> getBulletPositionsAndDirection();
-    /**
-     * Return the position and the direction of a tank indentified by a player.
-     * @param player the player
-     * @return the position and the direction
-     */
-    public Pair<P2d, Directions> getTankPositionAndDirection(final Player player);
-    /**
-     * Return the first Player.
-     * @return the Player
-     */
-    public Player getFirstPlayer();
-    /**
-     * Return the second Player.
-     * @return the player
-     */
-    public Player getSecondPlayer();
-    /**
-     * Return the score of the two player.
-     * @return the score
-     */
-    public Pair<Integer, Integer> getScore(); //0-0 1-0
+import it.unibo.tankBattle.common.input.api.Directions;
+import it.unibo.tankBattle.model.gameObject.api.object.GameObject;
+
+public interface GameState {
+
+    public void createWorld();
+
+    public void update(Double time);
+
+    public void shot(int player);
+
+    public void setDirection(Directions direction, int player);
+
+    public Stream<GameObject> getTanks();
+
+    public Stream<GameObject> getBullets();
+
+    public Stream<GameObject> getWalls();
+
+    public void addGameStateListener(/*WorldEventListener listener*/);
 }
--- src/main/java/it/unibo/tankBattle/model/gameState/impl/GameStateImpl.java
+++ src/main/java/it/unibo/tankBattle/model/gameState/impl/GameStateImpl.java
@@ -1,99 +1,65 @@
 package it.unibo.tankBattle.model.gameState.impl;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-import it.unibo.tankBattle.common.P2d;
-import it.unibo.tankBattle.common.Pair;
+import java.util.stream.Stream;
+
 import it.unibo.tankBattle.common.input.api.Directions;
-import it.unibo.tankBattle.controller.api.GameEngine;
+import it.unibo.tankBattle.model.gameObject.api.object.GameObject;
 import it.unibo.tankBattle.model.gameState.api.GameState;
-import it.unibo.tankBattle.model.gameState.api.Player;
+import it.unibo.tankBattle.model.world.api.FactoryWorld;
 import it.unibo.tankBattle.model.world.api.World;
 import it.unibo.tankBattle.model.world.impl.FactoryWorldImpl;
 
 public class GameStateImpl implements GameState {
+    private final FactoryWorld factoryWorld;
+    private World world = null;
+    //private WorldEventListener listener = null;
 
-    private final World world;
-    private final FactoryWorldImpl factory;
-    private final Player playerOne;
-    private final Player playerTwo;
-    private final GameEngine controller;
-
-    public GameStateImpl(final GameEngine controller) {
-        factory = new FactoryWorldImpl(this);
-        playerOne = new PlayerImpl();
-        playerTwo = new PlayerImpl();
-        this.world = factory.simpleWorld(playerOne, playerTwo);
-        this.controller = controller;
-
+    public GameStateImpl() {
+        factoryWorld = new FactoryWorldImpl();
     }
 
     @Override
-    public Pair<Integer, Integer> getScore() {
-        return new Pair<>(playerOne.getScore(), playerTwo.getScore());
+    public void createWorld() {
+        world = factoryWorld.simpleWorld();
     }
 
     @Override
-    public void update() {
-        this.world.update();
+    public void update(Double time) {
+        world.getEntities().forEach(g -> g.update(time));
     }
 
     @Override
-    public void resolveEvents(final Set<Pair<P2d, P2d>> events) {
-        events.forEach(p -> world.collision(p.getX(), p.getY()));
-    }
-
-    @Override
-    public void endGame(final Player player) {
-        player.incScore();
-        controller.endgame();
-    }
-
-    @Override
-    public void input() {
+    public void shot(int player) {
         // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'shot'");
     }
 
     @Override
-    public Player getFirstPlayer() {
-        return playerOne;
+    public void setDirection(Directions direction, int player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setDirection'");
     }
 
     @Override
-    public Player getSecondPlayer() {
-        return playerTwo;
-        
+    public Stream<GameObject> getTanks() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTanks'");
     }
 
     @Override
-    public Set<Pair<P2d, Integer>> getPositionsAndLength() {
-        return world
-            .getEntities()
-            .stream()
-            .map(obj -> new Pair<>(obj.getPosition(), obj.getLength()))
-            .collect(Collectors.toSet());
+    public Stream<GameObject> getBullets() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBullets'");
     }
 
     @Override
-    public Set<P2d> getWallPositions() {
-        return world
-            .getWalls()
-            .stream()
-            .map(wall -> wall.getPosition())
-            .collect(Collectors.toSet());
+    public Stream<GameObject> getWalls() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWalls'");
     }
 
     @Override
-    public Set<Pair<P2d, Directions>> getBulletPositionsAndDirection() {
-        return world
-            .getBullets()
-            .stream()
-            .map(bullet -> new Pair<>(bullet.getPosition(), bullet.getDirection()))
-            .collect(Collectors.toSet());
-    }
-
-    @Override
-    public Pair<P2d, Directions> getTankPositionAndDirection(final Player player) {
-        return new Pair<>(world.getTank(player).getPosition(), world.getTank(player).getDirection());
-    }
+    public void addGameStateListener(/*final WorldEventListener listener*/) {
+        //this.listener = listener;
+    }   
 }
