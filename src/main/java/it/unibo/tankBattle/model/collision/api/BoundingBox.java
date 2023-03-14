65ebd0c93922c495327bad4d1a52057e428182b5
Lucrezia Rettori <lucrezia.rettori@studio.unibo.it>
2023-03-14 17:02:50 +0100

    create BoundingBox interface and RectangularBoundingBox implementation

--- src/main/java/it/unibo/tankBattle/model/collision/api/BoundingBox.java
+++ src/main/java/it/unibo/tankBattle/model/collision/api/BoundingBox.java
@@ -2,22 +2,12 @@ package it.unibo.tankBattle.model.collision.api;
 
 import it.unibo.tankBattle.common.P2d;
 
-public class BoundingBox {
-
-    private final P2d center;
-    private final int length;
+public interface BoundingBox {
     
-    public BoundingBox(P2d center, int length) {
-        this.center = center;
-        this.length = length;
-    }
+    public P2d getCenter();
 
-    public P2d getCenter() {
-        return this.center;
-    }
+    public double getLength();
 
-    public int getLength() {
-        return this.length;
-    }
+    public double getWidth();
 
 }
