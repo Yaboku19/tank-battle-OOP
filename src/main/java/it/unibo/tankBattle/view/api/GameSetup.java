7fc14e25cd48846bb5ddb6c7d24de697ab58fd31
Tomas Ventrucci <tomas.ventrucci@studio.unibo.it>
2023-03-21 00:19:55 +0100

    create GameSetup interface to choose gaem settings

---
+++ src/main/java/it/unibo/tankBattle/view/api/GameSetup.java
@@ -0,0 +1,14 @@
+package it.unibo.tankBattle.view.api;
+
+public interface GameSetup {
+    
+    String getName();
+
+    Integer getSpeed();
+
+    Integer getLife();
+
+    /**
+     * and other
+     */
+}
