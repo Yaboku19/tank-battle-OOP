dc0d3028db78c3efd624440f2ea692e4a8f3d3f6
Tomas Ventrucci <tomas.ventrucci@studio.unibo.it>
2023-03-22 18:21:47 +0100

    implements read xml in controller

--- src/main/java/it/unibo/tankBattle/controller/api/GameEngine.java
+++ src/main/java/it/unibo/tankBattle/controller/api/GameEngine.java
@@ -3,10 +3,6 @@ package it.unibo.tankBattle.controller.api;
 import it.unibo.tankBattle.common.input.api.Command;
 
 public interface GameEngine extends Runnable{
-    /**
-     * It is called at the beginin by TankBattle.
-     */
-    public void play();
     
     public void startGame();
 
--- src/main/java/it/unibo/tankBattle/controller/impl/BasicGameEngine.java
+++ src/main/java/it/unibo/tankBattle/controller/impl/BasicGameEngine.java
@@ -4,42 +4,34 @@ import java.util.LinkedList;
 import java.util.Queue;
 import it.unibo.tankBattle.common.input.api.Command;
 import it.unibo.tankBattle.controller.api.GameEngine;
+import it.unibo.tankBattle.controller.api.ObjectsManager;
 import it.unibo.tankBattle.controller.api.Player;
 import it.unibo.tankBattle.controller.api.WorldEventListener;
 import it.unibo.tankBattle.model.gameState.api.GameState;
 import it.unibo.tankBattle.model.gameState.impl.GameStateImpl;
 import it.unibo.tankBattle.view.api.View;
+import it.unibo.tankBattle.view.impl.javafx.controller.ChooseMenu;
 
 public class BasicGameEngine implements GameEngine, WorldEventListener {
     private long period = 20;
     private final View view;
     private final GameState model;
     private final Queue<Command> commandQueue = new LinkedList<>();
-    //private HashMap<Player,InputController> controllers;
     private Thread thread;
     private Boolean isOver = false;
     private Player firstPlayer = null;
     private Player secondPlayer = null;
+    private final ObjectsManager<Integer,Integer> objectsManager;
+    private ChooseMenu settingsViewController = null;
 
     public BasicGameEngine(final View view) {
         thread = new Thread(this);
         this.view = view;
         view.setController(this);
         model = new GameStateImpl(this);
+        objectsManager = new ObjectsManagerImpl<Integer,Integer>("filePath");
+        //objectsManager.readVirus();
     }
-
-    @Override
-    public void play() {
-        /*initGame();
-        view.setVisible(true);
-        view.bugSolve();*/
-    }
-
-    /*private void initGame(){
-        controllers = new HashMap<Player,InputController>();
-        controllers.put(model.getFirstPlayer(), view.getInputControllerPlayer1());
-        controllers.put(model.getSecondPlayer(), view.getInputControllerPlayer2());
-    }*/
 
     @Override
     public void startGame() {
@@ -130,5 +122,9 @@ public class BasicGameEngine implements GameEngine, WorldEventListener {
         //startGame();
         System.out.println("aaa");
         loop();
+    }
+
+    public void setSettingsViewController(ChooseMenu settingsViewController){
+        this.settingsViewController = settingsViewController;
     }
 }
