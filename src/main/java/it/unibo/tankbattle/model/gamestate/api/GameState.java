61a14b8dd2abd2dad87b255c1c8d1ad8cb819f96
Emanuele_Martelli <emanuele.martelli5studio.unibo.it>
2023-03-29 12:29:35 +0200

    final change

---
+++ src/main/java/it/unibo/tankbattle/model/gamestate/api/GameState.java
@@ -0,0 +1,46 @@
+package it.unibo.tankbattle.model.gamestate.api;
+
+import java.util.stream.Stream;
+
+import it.unibo.tankbattle.common.Transform;
+import it.unibo.tankbattle.controller.api.Player;
+import it.unibo.tankbattle.model.gamesetup.impl.MapData;
+/**
+ * javadock.
+ */
+public interface GameState extends CommandListener {
+    /**
+     * javadock.
+     * @param firstPlayer param
+     * @param secondPlayer param
+     * @param dataList param
+     */
+    void createWorld(Player firstPlayer, Player secondPlayer, MapData dataList);
+    /**
+     * javadock.
+     * @param time param
+     */
+    void update(Double time);
+    /**
+     * javadock.
+     * @return param
+     */
+    Stream<Transform> getBulletsTrasform();
+    /**
+     * javsdock.
+     * @return param
+     */
+    Stream<Transform> getWallsTrasform();
+    /**
+     * javadock.
+     * @param player param
+     * @return return
+     */
+    Transform getTankTrasform(Player player);
+    /**
+     * javadock.
+     * @param player param
+     * @return return
+     */
+    int getTankLife(Player player);
+}
